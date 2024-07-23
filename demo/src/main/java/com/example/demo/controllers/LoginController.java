297781bd31146a8ae3d961384654f1aee13cbb91
Mirado <hei.mirado@gmail.com>
2024-07-23 14:56:51 +0300

    feat: oidc for more information

--- demo/src/main/java/com/example/demo/controllers/LoginController.java
+++ demo/src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,13 +34,13 @@ public class LoginController {
         return "Welcome, Admin";
     }
 
-    @GetMapping("/*")
-    public String getUserInfo(Principal user) {
+    @GetMapping("/")
+    public String getUserInfo(Principal user, @AuthenticationPrincipal OidcUser oidcUser) {
         StringBuffer userInfo = new StringBuffer();
         if(user instanceof UsernamePasswordAuthenticationToken){
             userInfo.append(getUsernamePasswordLoginInfo(user));
         }else if(user instanceof OAuth2AuthenticationToken){
-            userInfo.append(getOauth2LoginInfo(user));
+            userInfo.append(getOauth2LoginInfo(user, oidcUser));
         }
         return userInfo.toString();
     }
@@ -54,16 +57,29 @@ public class LoginController {
         return usernameInfo;
     }
 
-    private StringBuffer getOauth2LoginInfo(Principal user){
+    private StringBuffer getOauth2LoginInfo(Principal user, OidcUser oidcUser){
         StringBuffer protectedInfo = new StringBuffer();
         OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) user;
         OAuth2AuthorizedClient authClient = this.authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+
         if(authToken.isAuthenticated()){
             Map<String, Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
             String userToken = authClient.getAccessToken().getTokenValue();
             protectedInfo.append("Welcome, " + userAttributes.get("name") + "<br><br>");
             protectedInfo.append("e-mail: " + userAttributes.get("email") + "<br><br>");
             protectedInfo.append("Access token: " + userToken);
+
+            if (oidcUser != null) {
+                OidcIdToken idToken = oidcUser.getIdToken();
+                if (idToken != null) {
+                    protectedInfo.append("idToken value: " + idToken.getTokenValue() + "<br><br>");
+                    protectedInfo.append("Token mapped values <br><br>");
+                    Map<String, Object> claims = idToken.getClaims();
+                    for (String key : claims.keySet()) {
+                        protectedInfo.append("  " + key + ": " + claims.get(key) + "<br>");
+                    }
+                }
+            }
         }else {
             protectedInfo.append("NA");
         }
